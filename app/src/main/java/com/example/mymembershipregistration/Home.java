ab1f8657d44c586929dbc1f7fab0eb13b2bc72f6
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-09-08 19:57:14 +0800

    Fix: include View objects and View.OnClickListener in Home.java

--- app/src/main/java/com/example/mymembershipregistration/Home.java
+++ app/src/main/java/com/example/mymembershipregistration/Home.java
@@ -1,6 +1,8 @@
 package com.example.mymembershipregistration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
+
+    TextView usernameTv;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,10 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
