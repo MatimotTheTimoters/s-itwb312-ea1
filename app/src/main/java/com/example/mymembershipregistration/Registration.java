16088721b2ac368791adc3cfc057421523e185f5
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-09-08 19:38:33 +0800

    Feat, Chore: create exportEditTextValues method, set birthdayEt to lowercase
    What:
    - BirthdayEt to birthdayEt

--- app/src/main/java/com/example/mymembershipregistration/Registration.java
+++ app/src/main/java/com/example/mymembershipregistration/Registration.java
@@ -1,5 +1,6 @@
 package com.example.mymembershipregistration;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class Registration extends AppCompatActivity implements View.OnClickListener {
 
     EditText firstNameEt, lastNameEt,
-             emailAddressEt, BirthdayEt,
+             emailAddressEt, birthdayEt,
              usernameEt, passwordEt;
     Button registerBtn;
 
@@ -28,11 +29,32 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        registerBtn = findViewById(R.id.registerBtn);
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    public void registerBtnOnClick(View v) {
+        exportEditTextValues();
+    }
+
+    public void exportEditTextValues() {
+        Login login = new Login();
+        Intent exportIntent = new Intent(Registration.this, Login.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("firstNameEt", firstNameEt.getText().toString());
+        bundle.putString("lastNameEt", lastNameEt.getText().toString());
+        bundle.putString("emailAddressEt", emailAddressEt.getText().toString());
+        bundle.putString("birthdayEt", birthdayEt.getText().toString());
+        bundle.putString("usernameEt", usernameEt.getText().toString());
+        bundle.putString("passwordEt", passwordEt.getText().toString());
+        exportIntent.putExtras(bundle);
+        startActivity(exportIntent);
+        finish();
     }
 
     public void getEditTextId(EditText et, int id) {
